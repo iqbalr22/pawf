09a999d6947376a0d0a253c5aab00b2ffa356e23
iqbalr22 <iqbalramadan221101@gmail.com>
2026-04-20 15:49:57 +0700

    blog

---
+++ project3/app/Models/PostModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PostModel extends Model
+{
+    protected $table            = 'posts';
+    protected $primaryKey       = 'id';
+
+    protected $useAutoIncrement = true;
+    protected $allowedFields    = ['title', 'content', 'status', 'author', 'slug'];
+}
+
